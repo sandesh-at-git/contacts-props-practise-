4d17f1a4e417fa8f2b3ef8539f68f1c129e2320f
Sandesh-At-Git <sandeshpawar@aca.edu.in>
2021-11-21 15:53:06 +0530

    initial commit

---
+++ src/contacts.js
@@ -0,0 +1,52 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    imgURL:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone: "+123 456 789",
+    email: "b@beyonce.com",
+  },
+  {
+    name: "Jack Bauer",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com",
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com",
+  },
+  {
+    name: "Superman",
+    imgURL:
+      "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fjvchamary%2Ffiles%2F2016%2F03%2Fman_of_steel-1200x800.jpg",
+    phone: "+91899304343",
+    email: "Superman1@gmail.com",
+  },
+  {
+    name: "Brock Lesner",
+    imgURL:
+      "https://www.wwe.com/f/styles/og_image/public/rd-talent/Bio/Brock_Lesnar_bio.png",
+    phone: "+43434353",
+    email: "brockLes33@gmail.com",
+  },
+  {
+    name: "Batman",
+    imgURL:
+      "https://variety.com/wp-content/uploads/2017/09/batman.jpg?w=681&h=383&crop=1",
+    phone: "+43434353",
+    email: "brockLes33@gmail.com",
+  },
+  {
+    name: "Venom",
+    imgURL:
+      "https://www.cnet.com/a/img/C9tvLncYlNGseoqOELqUB8nIEA4=/1200x675/2021/09/03/afa4abf1-ea46-45bf-b4d0-84259920a236/qlwgiefucodivdzjgil7.jpg",
+    phone: "+43984274",
+    email: "Venom11@gmail.com",
+  },
+];
+export default contacts;
---
+++ src/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import Contact from "./contacts";
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Contacts</h1>
+    <App
+      name={Contact[0].name}
+      img={Contact[0].imgURL}
+      phone={Contact[0].phone}
+      email={Contact[0].email}
+    />
+    <App
+      name={Contact[1].name}
+      img={Contact[1].imgURL}
+      phone={Contact[1].phone}
+      email={Contact[1].email}
+    />
+    <App
+      name={Contact[2].name}
+      img={Contact[2].imgURL}
+      phone={Contact[2].phone}
+      email={Contact[2].email}
+    />
+    <App
+      name={Contact[3].name}
+      img={Contact[3].imgURL}
+      phone={Contact[3].phone}
+      email={Contact[3].email}
+    />
+    <App
+      name={Contact[4].name}
+      img={Contact[4].imgURL}
+      phone={Contact[4].phone}
+      email={Contact[4].email}
+    />
+    <App
+      name={Contact[5].name}
+      img={Contact[5].imgURL}
+      phone={Contact[5].phone}
+      email={Contact[5].email}
+    />
+    <App
+      name={Contact[6].name}
+      img={Contact[6].imgURL}
+      phone={Contact[6].phone}
+      email={Contact[6].email}
+    />
+
+  </div>,
+  document.getElementById("root")
+);
+
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
+//2. Extract the contact card as a reusable Card component.
+//3. Use props to render the default Beyonce contact card
+//so the Card component can be reused for other contacts.
+//4. Import the contacts.js file to create card components.
